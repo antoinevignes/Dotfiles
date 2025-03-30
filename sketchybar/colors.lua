ae313f9689ba0aa046207f4172506271e5d6e5aa
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-03-30 14:28:08 +0200

    First commit

---
+++ sketchybar/colors.lua
@@ -0,0 +1,28 @@
+return {
+  black = 0xff181926,
+  white = 0xffcad3f5,
+  red = 0xffed8796,
+  green = 0xffa6da95,
+  blue = 0xff8aadf4,
+  yellow = 0xffeed49f,
+  orange = 0xfff5a97f,
+  magenta = 0xffc6a0f6,
+  grey = 0xff6e738d,
+  transparent = 0x00000000,
+
+  bar = {
+    bg = 0x00000000,
+    border = 0xff2c2e34,
+  },
+  popup = {
+    bg = 0xff24273a,
+    border = 0xff6e738d,
+  },
+  bg1 = 0xff24273a,
+  bg2 = 0xff363a4f,
+
+  with_alpha = function(color, alpha)
+    if alpha > 1.0 or alpha < 0.0 then return color end
+    return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
+  end,
+}
